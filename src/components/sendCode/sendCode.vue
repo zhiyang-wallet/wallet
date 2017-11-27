6750cbaeb92ac0678fd3083df4b319cc81ea71a9
bin.chen <25822321@qq.com>
2017-11-27 11:31:05 +0800

    注册代码

---
+++ src/components/sendCode/sendCode.vue
@@ -0,0 +1,18 @@
+<template>
+
+      <div class="sendCode">
+        <mt-button type="default">获取短信验证码</mt-button>
+      </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+
+
+
+
+</style>
