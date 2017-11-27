ddb276697c9dac928cb188c9b1b329c72d88d1fb
bin.chen <25822321@qq.com>
2017-11-27 16:33:59 +0800

    累计收益 页面

---
+++ src/page/accumulated.vue
@@ -0,0 +1,44 @@
+<template>
+
+  <div class="main vm-wrap vm-bg_01">
+    <ryt-header :title="headerTitle" :backUrl="backUrl"></ryt-header>
+    <div class="accumulated-top">
+      <p class="txt1">累计收益（元）</p>
+      <p class="txt2">10000.1</p>
+      <a class="ryt-help help"></a>
+    </div>
+    <p class="field-title">近39日收益展示</p>
+    <div class="accumulated-list" >
+          <accumulatedList v-for="item in data"></accumulatedList>
+    </div>
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  import header from '../components/header/header'
+
+  let accumulatedList = {
+
+    template:' <div class="accumulated-item"><div class="bar"><span class="vm-left">2017-10-10</span><span class="vm-right">1.01</span></div></div>'
+
+  }
+
+  export default {
+    components: {
+      'ryt-header': header,
+      accumulatedList
+    },
+    data() {
+      return {
+        backUrl: '/',
+        headerTitle: '累计收益',
+        data:[1,2,3,4,5]
+      };
+    }
+  }
+</script>
+
+<style lang="scss" rel="stylesheet/scss">
+  @import "../style/vm";
+</style>
